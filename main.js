0852a8b5b619d1c0777333413c8304f05b897151
hecthorl <hlvm03@gmail.com>
2021-08-07 17:21:22 -0500

    modo desktop y mobile

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 const toggleMenuBtn = document.querySelector("#burger-container");
 const menu = document.querySelector(".navigation");
+const loader = document.querySelector(".loader-dom");
+const header = document.querySelector("header");
 
 toggleMenuBtn.onclick = () => {
-  menu.classList.toggle("active-menu");
+  menu.classList.add("active-menu");
 };
-menu.onclick = () => menu.classList.remove("active-menu");
+menu.onclick = () => {
+  console.log("asdasd");
+  menu.classList.remove("active-menu");
+};
+
+document.body.onload = () => {
+  console.log("termino la carga");
+  loader.remove();
+};
+
+document.body.onscroll = (e) => {
+  const { top, x, y } = header.getBoundingClientRect();
+  console.log({ x, y });
+  if (!top) {
+    header.classList.add("bg-dark");
+  } else {
+    header.classList.remove("bg-dark");
+  }
+};
