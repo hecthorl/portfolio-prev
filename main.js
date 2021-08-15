3db9e0586f7f8b53641e6d00a499acf7a7a27787
hecthorl <hlvm03@gmail.com>
2021-08-15 15:48:05 -0500

    casi casi

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import randomBG from "./randomBG";
 
 const loader = document.querySelector(".loader-dom");
-const $ = (className) => document.querySelector(className);
+const $ = (className) => document.querySelectorAll(className);
 
 document.body.onload = () => {
   console.log("termino la carga");
@@ -10,14 +10,18 @@ document.body.onload = () => {
 
 const speedChange = 4e3;
 
+const funcionDeTipoFlecha = (item) => {
+  item.style.backgroundColor = `#${randomBG()}`;
+};
+
 setInterval(() => {
-  $(".circle-1").style.backgroundColor = `#${randomBG()}`;
+  $(".circle-1").forEach(funcionDeTipoFlecha);
 }, speedChange);
 setInterval(() => {
-  $(".circle-2").style.backgroundColor = `#${randomBG()}`;
+  $(".circle-2").forEach(funcionDeTipoFlecha);
 }, speedChange);
 setInterval(() => {
-  $(".circle-3").style.backgroundColor = `#${randomBG()}`;
+  $(".circle-3").forEach(funcionDeTipoFlecha);
 }, speedChange);
 
 // const d_value = 520;
