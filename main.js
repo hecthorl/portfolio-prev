ffd7df3c8c015fbdf31f8191f92e7b72a73bdc8a
hecthorl <hlvm03@gmail.com>
2021-08-23 14:50:11 -0500

    90%

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 import randomBG from "./randomBG";
 
 const loader = document.querySelector(".loader-dom");
+// const btnContainer = document.querySelector("#toBtn");
+// const fakeScreen = document.createElement("div");
+// const popup = document.createElement("div");
 const nav = document.querySelector(".navigation");
-const btnContact = document.querySelector(".navigation button");
+// const btnContact = document.querySelector(".navigation button");
+
 const $ = (className) => document.querySelectorAll(className);
 
 document.body.onload = () => {
   console.log("termino la carga");
+  document.body.style.overflow = "auto";
   loader.remove();
 };
+
+// btnContact.onclick = () => {
+//   popup.classList.add("popUp-contact");
+//   popup.innerHTML = `<div>
+//       <span>info</span>
+//       <span>info</span>
+//   </div>`;
+
+//   btnContainer.appendChild(popup);
+
+//   fakeScreen.setAttribute("id", "test");
+//   document.body.appendChild(fakeScreen);
+// };
+
+// fakeScreen.onclick = () => {
+//   popup.remove();
+//   fakeScreen.remove();
+// };
 
 const speedChange = 4e3;
 
